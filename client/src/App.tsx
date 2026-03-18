ea7feb80b774e79e0b4d1c571db189ece3fa4de3
Misho1019 <mikailtorres99@gmail.com>
2026-03-18 04:46:17 +0200

    Initialize frontend with Vite, React, Tailwind and charts setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App
